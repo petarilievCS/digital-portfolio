4532e9f0614725351295e4c054cab1c0cf059aed
Petar Iliev <petariliev@utexas.edu>
2023-04-19 14:10:46 -0500

    Add Timeline

--- digital-portfolio/src/components/Portfolio.jsx
+++ digital-portfolio/src/components/Portfolio.jsx
@@ -4,7 +4,7 @@ import PortfolioItem from './PortfolioItem';
 
 function Portfolio() {
    return (
-      <div className="max-w-5xl flex flex-col md:flex-row items-center justify-center m-auto">
+      <div className="max-w-6xl flex flex-col md:flex-row items-center justify-center m-auto">
          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-8">
             {portfolio.map(project => (
                <PortfolioItem 
---
+++ digital-portfolio/src/data/timeline.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        year: "Today",
+        title: "Software Engineer Intern",
+        duration: "3 months",
+        details:
+            "I'm currently interning at ServiceNow as a Software Engineer intern on the iOS Engineering team. I'm working on the ServiceNow iOS App and I'm learning more about Swift, SwiftUI, UIKit and Cocoa Pods, as well as the ServiceNow platform."
+    },
+    {
+        year: "Today",
+        title: "Independent iOS Developer",
+        duration: "1 year",
+        details:
+            "I'm a solo independent iOS developer who is profficient in Swift (UIKit, SwiftUI, CoreData), Flutter and Firebase. I have developed and published 3 app on the App Store and I'm looking to expand my skillset and develop more apps."
+    },
+    {
+        year: "2022",
+        title: "Quality Engineer Intern",
+        duration: "3 monhts",
+        details:
+            "I interned at ServiceNow as a Quality Engineer intern on the Platform Evolution team. I wrote automated tests in Java using JUnit and Selenium. I also developed a Chrome extension that automates the testing of the ServiceNow platform."
+    },
+    {
+        year: "2021",
+        title: "Bachelor's Degree in Computer Science",
+        duration: "4 years",
+        details:
+            "I started studying Computer Science at the University of Texas at Austin. I learned about data structures, computer architecture, operating systems, software engineerin and object-oriented programming."
+    },
+    {
+        year: "2020",
+        title: "First Line of Code",
+        duration: "the beginning",
+        details:
+            "I wrote my first line of code in Python, not knowing that it would lead me to studying Computer Science in college. Soon after I took an AP Computer Science class and started learning Java."
+    },
+]
